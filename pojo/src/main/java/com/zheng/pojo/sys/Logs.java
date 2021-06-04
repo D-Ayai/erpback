1546b017bf9ac97b9cc16039bf67d998b23a05fa
D-Ayai <2295924001@qq.com>
2021-06-04 16:43:25 +0800

    建项目

--- pojo/src/main/java/com/zheng/pojo/sys/Logs.java
+++ pojo/src/main/java/com/zheng/pojo/sys/Logs.java
@@ -11,65 +11,11 @@ public class Logs {
 
     private Date logDate;
 
-    private String class;
+    private String type;
 
     private String method;
 
     private String msg;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getLoginId() {
-        return loginId;
-    }
-
-    public void setLoginId(String loginId) {
-        this.loginId = loginId == null ? null : loginId.trim();
-    }
-
-    public String getPriority() {
-        return priority;
-    }
-
-    public void setPriority(String priority) {
-        this.priority = priority == null ? null : priority.trim();
-    }
-
-    public Date getLogDate() {
-        return logDate;
-    }
-
-    public void setLogDate(Date logDate) {
-        this.logDate = logDate;
-    }
-
-    public String getClass() {
-        return class;
-    }
-
-    public void setClass(String class) {
-        this.class = class == null ? null : class.trim();
-    }
-
-    public String getMethod() {
-        return method;
-    }
-
-    public void setMethod(String method) {
-        this.method = method == null ? null : method.trim();
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg == null ? null : msg.trim();
-    }
 }
